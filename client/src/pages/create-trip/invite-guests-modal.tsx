729b7340024a77bed5e9ffacc60c6a992cf82e41
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2024-09-15 21:00:13 -0300

    feat: add component button

--- client/src/pages/create-trip/invite-guests-modal.tsx
+++ client/src/pages/create-trip/invite-guests-modal.tsx
@@ -1,5 +1,6 @@
 import { X, AtSign, Plus } from "lucide-react";
 import { FormEvent } from "react";
+import { Button } from "../../components/button";
 
 interface InviteGuestsModalProps {
   closeGuestsModal: () => void
@@ -51,10 +52,10 @@ export function InviteGuestsModal({
             />
           </div>
 
-          <button type='submit' className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+          <Button variant="primary" type="submit">
             Convidar
             <Plus className='size-5 text-lime-950' />
-          </button>
+          </Button>
         </form>
       </div>
     </div>
--- client/src/pages/create-trip/steps/invite-guests-step.tsx
+++ client/src/pages/create-trip/steps/invite-guests-step.tsx
@@ -1,4 +1,5 @@
 import { UserRoundPlus, ArrowRight } from "lucide-react";
+import { Button } from "../../../components/button";
 
 interface InviteGuestsStepProps {
   emailsToInvite: string[]
@@ -13,21 +14,21 @@ export function InviteGuestsStep({
 }: InviteGuestsStepProps) {
   return (
     <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-    <button type='button' onClick={openGuestsModal} className='flex items-center gap-2 flex-1 text-left'>
-      <UserRoundPlus className='size-5 text-zinc-400' />
-      {emailsToInvite.length > 0 ? (
-        <p className='text-lg text-zinc-100 outline-none flex-1'>{emailsToInvite.length} pessoas convidada(s)</p>
-      ) : (
-        <span className="text-lg text-zinc-400 outline-none flex-1">Quem estará na viagem?</span>
-      )}
-    </button>
+      <button type='button' onClick={openGuestsModal} className='flex items-center gap-2 flex-1 text-left'>
+        <UserRoundPlus className='size-5 text-zinc-400' />
+        {emailsToInvite.length > 0 ? (
+          <p className='text-lg text-zinc-100 outline-none flex-1'>{emailsToInvite.length} pessoas convidada(s)</p>
+        ) : (
+          <span className="text-lg text-zinc-400 outline-none flex-1">Quem estará na viagem?</span>
+        )}
+      </button>
 
-    <div className='w-px h-6 bg-zinc-800' />
+      <div className='w-px h-6 bg-zinc-800' />
 
-    <button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400' onClick={openConfirmTripModal}>
-      Confirmar viagem
-      <ArrowRight className='size-5' />
-    </button>
-  </div>
+      <Button variant="primary" onClick={openConfirmTripModal}>
+        Confirmar viagem
+        <ArrowRight className='size-5' />
+      </Button>
+    </div>
   )
 }
